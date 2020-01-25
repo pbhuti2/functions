68bc82cbf3f42eccbf6930a33a83749f06b2d071
pbhuti2 <55327021+pbhuti2@users.noreply.github.com>
2020-01-25 14:05:54 -0600

    Initial commit

---
+++ practiceFunctions.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 16, 2019
+
+@author: ITAUser
+'''
+def addition(num1,num2):
+    sumOfTwoNumbers= num1 + num2
+    return sumOfTwoNumbers
+
+sumOfTwoNumbers = addition(num1, num2)
+
+def sum_three_numbers(num1, num2, num3):
+    sumOfNumbers = num1 + num2 + num3 
+    return= sumOfNumbers
